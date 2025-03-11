db1efede8ad30aafb1fb7b75d41adfb26b35f2a9
Jordy Slagter <slagterj2001@gmail.com>
2025-03-11 15:40:10 +0100

    work more on the cards page

--- lib/pages/cards.dart
+++ lib/pages/cards.dart
@@ -6,8 +6,27 @@ class CardsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Kaartenspellen"), centerTitle: true),
-      body: const Center(child: Text("Kaartenspellen!")),
+      appBar: AppBar(title: const Text("Kaartenspellen"), centerTitle: true),
+      body: const Center(child: Text("Geen kaartenspellen gevonden...")),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed:
+            () => showDialog(
+              context: context,
+              builder:
+                  (context) => Dialog.fullscreen(child: createDeckDialog()),
+            ),
+      ),
+    );
+  }
+
+  Scaffold createDeckDialog() {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Voeg kaartenspel toe"),
+        centerTitle: true,
+      ),
+      body: const Center(child: Text("Voeg kaartenspel toe!")),
     );
   }
 }
