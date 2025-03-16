f4cdd78351f190ffd09a5f031ee6136e7f4b838d
Jordy Slagter <slagterj2001@gmail.com>
2025-03-16 12:54:33 +0100

    disable status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:provider/provider.dart";
 import "package:truth_or_drink/pages/home_page.dart";
 import "package:truth_or_drink/services/database.dart";
@@ -13,6 +14,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom],
+    );
+
     return Provider(
       create: (_) => AppDatabase(),
       child: MaterialApp(
